16c90ad99bb739e28f1c002381e45c72b94e1492
Nazar Kundys <nazarrisd@gmail.com>
2025-11-05 14:39:11 +0200

    Implements a rental cost calculation in `calculateRentalCost.js` with daily fees and discounts for extended rentals (3+ and 7+ days).

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_FEE = 40;
+  const totalCost = days * DAILY_FEE;
+
+  // discounts based on rented days [min day : discount amount]
+  const daysDiscount = {
+    3: 20,
+    7: 50,
+  };
+
+  // find the maximum applicable discount
+  const discount =
+    daysDiscount[Object.keys(daysDiscount).findLast((dd) => days >= dd)] || 0;
+
+  return totalCost - discount;
 }
 
 module.exports = calculateRentalCost;
